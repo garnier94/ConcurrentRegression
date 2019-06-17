25ff96876d980e8ca0ad2037b0e546c8c1fae275
Rémy Garnier <remy.garnier@cdiscount.com>
2019-06-17 17:00:08 +0200

    INGARCH(p) concurrents

--- src/Concurrent_INREG.py
+++ src/Concurrent_INREG.py
@@ -1,5 +1,5 @@
-
-import  numpy as np
+from math import log
+import numpy as np
 from INREG_model import INREG_model
 
 
@@ -19,8 +19,10 @@ def _find_max_intersection(series_1, series_2):
             i = j
     return [i_max, j_max]
 
-def _sum_period(X,i,j):
+
+def _sum_period(X, i, j):
     return sum(X[i:j])
+
 
 class Concurrent_INREG(INREG_model):
     def __init__(self, weight=1, season=100, **kwargs):
@@ -29,45 +31,61 @@ class Concurrent_INREG(INREG_model):
         self.season = season
         self.g = None
 
-        def aux(X, t):
-            gX = self.g([X[hist] / (self.season[t-self.horizon-hist] * weight) for hist in range(self.history)])
+        def aux(X, t, old):
+            gX = self.g([X[hist] / (self.season[t - self.horizon - hist] * weight) for hist in range(self.history)],
+                        old)
             weighted_sum = gX.dot(self.weight)
             return self.season[t] * self.weight * gX / weighted_sum
 
         self.f = aux
 
-    def estim_weight(self,X):
+    def estim_weight(self, X):
         M = X.shape[1]
         weight = np.ones(M)
-        for ind_j in range(1,M):
-            ecart =  np.array([_find_max_intersection(X[:,ind_i],X[:,ind_j]) for ind_i in range(ind_j)])
-            ind_i_max =  np.argmax(ecart[:,1] - ecart[:,0])
+        for ind_j in range(1, M):
+            num_csd = 0
 
-            i_max, j_max =  ecart[ind_i_max,0],ecart[ind_i_max,1]
-            if j_max -i_max  > 4:
-                S_i = _sum_period(X[:, ind_i_max], (i_max +1),(j_max-1))
-                S_j = _sum_period(X[:, ind_j], (i_max +1),(j_max-1))
-                weight[ind_j] = weight[ind_i_max] * S_j /S_i
-
+            ecart = np.array([_find_max_intersection(X[:, ind_i], X[:, ind_j]) for ind_i in range(ind_j)])
+            for ind_i in range(ind_j):
+                i_max, j_max = ecart[ind_i, 0], ecart[ind_i, 1]
+                if j_max - i_max > 4:
+                    S_i = _sum_period(X[:, ind_i], (i_max + 1), (j_max - 1))
+                    S_j = _sum_period(X[:, ind_j], (i_max + 1), (j_max - 1))
+                    weight[ind_j] += weight[ind_i] * S_j / S_i
+                    num_csd += 1
+            if num_csd > 0:
+                weight[ind_j] = weight[ind_j] / num_csd
         self.weight = weight
 
+
 class Concurrent_INAR(Concurrent_INREG):
-    """ INAR(1) model"""
-
-    def __init__(self,   **kwargs):
-        Concurrent_INREG.__init__(self, **kwargs)
-        self.parameters = np.array([1 for i in range(self.history)] + [0])
-        self.bound = [(1E-3,1E3) for i in range(self.history+1)]
-        self.g = lambda x: sum( [self.parameters[hist] * x[hist]  for hist in range(self.history)])+ self.parameters[self.history]
-
-
-class Concurrent_log_INAR(Concurrent_INREG):
-    """ INAR(1) model"""
+    """ INAR(p) model"""
 
     def __init__(self, **kwargs):
         Concurrent_INREG.__init__(self, **kwargs)
-        self.parameters = np.array([1, 0])
-        self.bound = ((1e-10, 1000), (0., 100))
-        self.g = lambda x: self.parameters[0] * np.log(1 + x) + self.parameters[1]
+        csta = kwargs.get('cst', True)
+        self.parameters = np.array([1 for i in range(self.history)])
+        self.bound = [(1E-3, 1E3) for i in range(self.history)]
+        self.g = lambda x, old: sum([self.parameters[hist] * x[hist] for hist in range(self.history)]) + csta
 
 
+class Concurrent_log_INAR(Concurrent_INREG):
+    """ INAR(p) model"""
+
+    def __init__(self, **kwargs):
+        Concurrent_INREG.__init__(self, **kwargs)
+        self.parameters = np.array([1 for i in range(self.history)])
+        self.bound = [(1E-3, 1E3) for i in range(self.history)]
+        self.g = lambda x, old: sum([self.parameters[hist] * np.log(1 + x[hist]) for hist in range(self.history)]) + 1
+
+
+class Concurrent_INGARCH(Concurrent_INREG):
+    """Concurrent INGARCH models"""
+
+    def __init__(self, **kwargs):
+        Concurrent_INREG.__init__(self, **kwargs)
+        cst = kwargs.get('cst', True)
+        self.parameters = np.array([1 for i in range(self.history * 2)])
+        self.bound = [(1E-3, 1E3) for i in range(self.history * 2)]
+        self.g = lambda x, old: sum([self.parameters[hist] * x[hist] for hist in range(self.history)]) + cst + sum(
+            [self.parameters[self.history + hist] * old[hist] for hist in range(self.history)])
--- src/INREG_model.py
+++ src/INREG_model.py
@@ -7,7 +7,7 @@ class INREG_model():
     """
     """
 
-    def __init__(self, horizon=1, history=1):
+    def __init__(self, horizon=1, history=1,**kwargs):
         self.f = None
         self.parameters = None
         self.horizon = horizon
@@ -21,17 +21,23 @@ class INREG_model():
         T, M = data.shape
 
         def aux(x, l):
-            if l != 0:
+            if x == 0:
+                return 0
+            elif l != 0:
                 return l - x * log(l)
             else:
                 return x
 
         vect_aux = np.vectorize(aux)
         current_log_likelihood = 0
+        ###
+        old_lambda = [data[self.history - 1 - i, :] for i in range(self.history)]
+
         for t in range(self.history - 1, T - self.horizon):
             X_t, X_t1 = [data[t - i, :] for i in range(self.history)], data[t + self.horizon, :]
-            lambda_t = self.f(X_t, t)
+            lambda_t = self.f(X_t, t, old_lambda)
             current_log_likelihood += sum(vect_aux(X_t1, lambda_t))
+            old_lambda = [lambda_t] + old_lambda[:-1]
         return current_log_likelihood
 
     def pred(self, data):
@@ -41,8 +47,14 @@ class INREG_model():
         :return:
         """
         T, M = data.shape
-        return np.concatenate([self.f([data[t - hist, :] for hist in range(self.history)], t) for t in
-                               range(self.history - 1, T)]).reshape(T-self.history + 1, M)
+
+        list_pred = list()
+        old_lambda = [data[self.history - 1 - i, :] for i in range(self.history)]
+        for t in range(self.history - 1, T):
+            new_lambda = self.f([data[t - hist, :] for hist in range(self.history)],t,old_lambda)
+            list_pred.append(new_lambda)
+            old_lambda = [new_lambda] + old_lambda[:-1]
+        return np.concatenate(list_pred).reshape(T-self.history + 1, M)
 
     def fit(self, mat, **kwargs):
         def aux(params):
@@ -59,4 +71,4 @@ class INAR(INREG_model):
     def __init__(self):
         self.parameters = np.array([1, 0])
         self.bound = ((1e-10, 10), (0., 100))
-        self.f = lambda x, t: self.parameters[0] * x[0] + self.parameters[1]
+        self.f = lambda x, t, old: self.parameters[0] * x[0] + self.parameters[1]
